10205599930383d9631281a7ebcf5e549669cdd6
kamil <kamilmaslanka212@gmail.com>
2025-07-25 01:15:56 +0200

    Loading spinner

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { RingLoader } from "react-spinners";
 
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <RingLoader />
+    </div>
+  );
 }
